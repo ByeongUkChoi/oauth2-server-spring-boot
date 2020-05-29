2dc8484e8d0a992b5b3006adfee2c1a3106c2e7f
ChoiBU <cbw0916@gmail.com>
2020-05-30 07:07:41 +0900

    Override configure

--- src/main/java/com/example/authserver/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/example/authserver/config/Oauth2AuthorizationConfig.java
@@ -1,10 +1,15 @@
 package com.example.authserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
 @EnableAuthorizationServer
 public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        super.configure(clients);
+    }
 }
