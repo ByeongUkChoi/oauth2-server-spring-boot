b097ab37bdf070cc5309fbc38c82c848a2aa13cb
ChoiBU <cbw0916@gmail.com>
2020-06-06 00:35:46 +0900

    Add Get Token test

--- src/main/java/com/example/authorizationserver/controller/AuthController.java
+++ src/main/java/com/example/authorizationserver/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.authorizationserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class AuthController {
     public void requestAuth(@RequestParam("client_id") String clientId,
                             @RequestParam("redirect_uri") String redirectUri,
                             @RequestParam("response_type") String responseType) {
-
+    }
+    @PostMapping("/oauth/token")
+    public void getToken(@RequestParam("grant_type") String grantType,
+                            @RequestParam("client_id") String clientId,
+                         @RequestParam("redirect_uri") String redirectUri,
+                         @RequestParam("code") String code) {
     }
 }
--- src/test/java/com/example/authorizationserver/controller/AuthControllerTest.java
+++ src/test/java/com/example/authorizationserver/controller/AuthControllerTest.java
@@ -4,15 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -27,13 +25,30 @@ public class AuthControllerTest {
 
         this.mockMvc.perform(get("/oauth/authorize")
                 .param("client_id", "testClientId")
-                .param("redirect_uri", "http://localhost/auth/login")
+                .param("redirect_uri", "http://localhost:8080/client")
                 .param("response_type", "code"))
                 .andExpect(status().isOk())
-                .andDo(document("auth", requestParameters(
+                .andDo(document("request-auth", requestParameters(
                         parameterWithName("client_id").description("앱 생성 시 발급 받은 REST API 키"),
                         parameterWithName("redirect_uri").description("코드를 리다이렉트 해줄 URI"),
                         parameterWithName("response_type").description("\"code\"로 고정")
+                )));
+    }
+
+    @Test
+    public void getTokenTest() throws Exception {
+
+        this.mockMvc.perform(post("/oauth/token")
+                .param("grant_type", "authorization_code")
+                .param("client_id", "testClientId")
+                .param("redirect_uri", "http://localhost:8080/client")
+                .param("code", "testCode"))
+                .andExpect(status().isOk())
+                .andDo(document("get-token", requestParameters(
+                        parameterWithName("grant_type").description("\"authorization_code\"로 고정"),
+                        parameterWithName("client_id").description("앱 생성 시 발급 받은 REST API"),
+                        parameterWithName("redirect_uri").description("코드가 리다이렉트된 URI"),
+                        parameterWithName("code").description("코드 받기 요청으로 얻은 인증 코드")
                 )));
     }
 
