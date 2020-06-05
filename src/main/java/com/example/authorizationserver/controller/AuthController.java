a67533f1cd85f43db63209f65a3e858d7aacb8f9
ChoiBU <cbw0916@gmail.com>
2020-06-05 09:44:18 +0900

    Add request auth code Test

--- src/main/java/com/example/authorizationserver/controller/AuthController.java
+++ src/main/java/com/example/authorizationserver/controller/AuthController.java
@@ -1,20 +1,16 @@
 package com.example.authorizationserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
 
 @RestController
 public class AuthController {
 
     @GetMapping("/oauth/authorize")
-    public void requestAuth() {
+    public void requestAuth(@RequestParam("client_id") String clientId,
+                            @RequestParam("redirect_uri") String redirectUri,
+                            @RequestParam("response_type") String responseType) {
 
-    }
-    @GetMapping("/")
-    public Map<String, Object> greeting() {
-        return Collections.singletonMap("message", "Hello, World");
     }
 }
--- src/test/java/com/example/authorizationserver/controller/AuthControllerTest.java
+++ src/test/java/com/example/authorizationserver/controller/AuthControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.authorizationserver;
+package com.example.authorizationserver.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,31 +6,35 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 @AutoConfigureRestDocs
-public class RestDocTest {
+public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void sample() throws Exception {
+    public void requestAuthTest() throws Exception {
 
-        this.mockMvc.perform(get("/"))
+        this.mockMvc.perform(get("/oauth/authorize")
+                .param("client_id", "testClientId")
+                .param("redirect_uri", "http://localhost/auth/login")
+                .param("response_type", "code"))
                 .andExpect(status().isOk())
-                .andDo(document("test",
-                        responseFields(
-                                fieldWithPath("message").type(JsonFieldType.STRING).description("메세지다")
-                        )
-                ));
+                .andDo(document("auth", requestParameters(
+                        parameterWithName("client_id").description("앱 생성 시 발급 받은 REST API 키"),
+                        parameterWithName("redirect_uri").description("코드를 리다이렉트 해줄 URI"),
+                        parameterWithName("response_type").description("\"code\"로 고정")
+                )));
     }
 
 }
