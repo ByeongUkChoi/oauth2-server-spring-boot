6b4b32e9f3af77956bb31b13ebe515432743b663
ChoiBU <cbw0916@gmail.com>
2020-06-09 07:55:02 +0900

    Add login

--- src/main/java/com/example/authorizationserver/controller/AuthenticationController.java
+++ src/main/java/com/example/authorizationserver/controller/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.example.authorizationserver.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * 로그인 컨트롤러
@@ -16,8 +19,10 @@ public class AuthenticationController {
      * @param continueUrl
      */
     @GetMapping("/login")
-    public String loginForm(@RequestParam(value = "continue", required = false) String continueUrl) {
+    public String loginForm(@RequestParam(value = "continue", required = false) String continueUrl,
+                            Model model) {
         // TODO: 로그인 페이지 반환
+        model.addAttribute("continue", continueUrl);
         return "login";
     }
 
@@ -25,6 +30,12 @@ public class AuthenticationController {
      * 로그인
      */
     @PostMapping("/authenticate")
-    public void login() {
+    public String login(@RequestParam("id") String id,
+                        @RequestParam("password") String password,
+                        @RequestParam("continue") String continueUrl,
+                        RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("code", "code123");
+        //TODO: redirect continueUrl
+        return "redirect:https://naver.com";
     }
 }
