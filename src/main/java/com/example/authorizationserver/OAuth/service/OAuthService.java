e132bb252c01e3daa9a79d47f8628d217c8b2b84
ChoiBU <cbw0916@gmail.com>
2020-07-06 21:51:43 +0900

    Add application.properties variables 토큰 만료 시간 환경변수로 사용

--- src/main/java/com/example/authorizationserver/OAuth/service/OAuthService.java
+++ src/main/java/com/example/authorizationserver/OAuth/service/OAuthService.java
@@ -5,60 +5,68 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.example.authorizationserver.OAuth.dao.AuthorizationCodeRepository;
 import com.example.authorizationserver.OAuth.domain.AuthorizationCode;
 import com.example.authorizationserver.OAuth.dto.TokenDto;
-import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.Charset;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Random;
 
 @Service
-@AllArgsConstructor
 public class OAuthService {
 
-        private AuthorizationCodeRepository authorizationCodeRepository;
 
-        /**
-         * AuthorizationCode를 생성하여 DB 저장하고 code 반환
-         * authorize_code 발급
-         */
-        public String getAuthorizationCode(long memberId, String clientId, String redirectUri) {
+    @Autowired
+    private AuthorizationCodeRepository authorizationCodeRepository;
 
-            // 만료 시간 설정. 현재 시간 + 60분
-            int currentTimestamp = (int) (System.currentTimeMillis() / 1000);
-            int expirationTimestamp = 60 * 60;  // authorize_code 만료 시간 60분 ( TODO: 상수로 분리 시켜야함)
+    //application.properties 사용
+    @Value("${authorizationServer.authorizationCode.expiresIn}")
+    private int authorizationCodeExpiresIn;
 
-            // 코드 랜덤으로 생성
-            String code = RandomStringUtils.randomAlphanumeric(86);
+    @Value("${authorizationServer.accessToken.expiresIn}")
+    private int accessTokenExpiresIn;
 
-            AuthorizationCode authorizationCode = AuthorizationCode.builder()
-                    .clientId(clientId)
-                    .memberId(memberId)    // TODO: test
-                    .redirectUri(redirectUri)
-                    .expires(currentTimestamp + expirationTimestamp)
-                    .code(code).build();
+    @Value("${authorizationServer.refreshToken.expiresIn}")
+    private int refreshTokenExpiresIn;
 
-            // authorize code insert
-            authorizationCodeRepository.save(authorizationCode);
+    /**
+     * AuthorizationCode를 생성하여 DB 저장하고 code 반환
+     * authorize_code 발급
+     */
+    public String getAuthorizationCode(long memberId, String clientId, String redirectUri) {
 
-            return code;
-        }
+        // 현재 시간 (타임스탬프 (초))
+        int currentTimestamp = (int) (System.currentTimeMillis() / 1000);
+
+        // 코드 랜덤으로 생성
+        String code = RandomStringUtils.randomAlphanumeric(86);
+
+        AuthorizationCode authorizationCode = AuthorizationCode.builder()
+                .clientId(clientId)
+                .memberId(memberId)
+                .redirectUri(redirectUri)
+                .expires(currentTimestamp + authorizationCodeExpiresIn)
+                .code(code).build();
+
+        // authorize code insert
+        authorizationCodeRepository.save(authorizationCode);
+
+        return code;
+    }
 
     /**
      * 토큰 발급
      * @return
      */
-	public TokenDto getToken() {
-        Algorithm algorithm = Algorithm.HMAC512("secret");
+    public TokenDto getToken() {
+        Algorithm algorithm = Algorithm.HMAC512("secret");  // TODO: sign값 상수로
         Date currentDate = new Date();
 
         // expires_in은 만료 시간
-
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(currentDate);
-        calendar.add(Calendar.SECOND, 3600);
+        calendar.add(Calendar.SECOND, accessTokenExpiresIn);
         Date expiredDate = calendar.getTime();
 
         String accessToken = JWT.create()
@@ -71,7 +79,7 @@ public class OAuthService {
                 .access_token(accessToken)
                 .token_type("bearer")
                 .refresh_token("this_is_refresh_token")
-                .expires_in(3600)
+                .expires_in(accessTokenExpiresIn)
                 .build();
-	}
+    }
 }
