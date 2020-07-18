f3f7d3860dfa756bb8d16566468832f4c90e29c3
ChoiBU <cbw0916@gmail.com>
2020-07-18 21:28:27 +0900

    Remove module 모듈을 먼저 만들기로 하였다.

--- oauth2-server-module/src/main/java/com/example/oauth2server/dto/TokenDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.oauth2server.dto;
-
-public class TokenDto {
-}
--- oauth2-server-module/src/main/java/com/example/oauth2server/repository/RefreshTokenRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.oauth2server.repository;
-
-public interface RefreshTokenRepository {
-    
-}
