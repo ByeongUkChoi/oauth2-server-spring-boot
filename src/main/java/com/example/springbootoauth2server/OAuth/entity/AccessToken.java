46f7e970e558c7796a04a33e1771a5fbd1e840f2
ChoiBU <cbw0916@gmail.com>
2020-08-04 00:10:52 +0900

    Add AccessToken.java

--- src/main/java/com/example/springbootoauth2server/OAuth/entity/AccessToken.java
+++ src/main/java/com/example/springbootoauth2server/OAuth/entity/AccessToken.java
@@ -1,25 +1,31 @@
 package com.example.springbootoauth2server.OAuth.entity;
 
-import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
- * CREATE TABLE oauth_refresh_tokens (
+ * CREATE TABLE oauth_access_tokens (
  *   token             VARCHAR(400)    NOT NULL PRIMARY KEY,
  *   client_id         VARCHAR(32)     NOT NULL,
  *   username          VARCHAR(40)     NOT NULL,
- *   access_token      VARCHAR(400),
  *   expired_at        TIMESTAMP       NOT NULL,
  *   created_at        TIMESTAMP       NOT NULL,
  * );
  */
-@Entity
-public class RefreshToken {
+public class AccessToken implements com.byeongukchoi.oauth2.server.entity.AccessToken {
     @Id
     private String token;
     private String clientId;
     private String username;
-    private String accessToken;
     private int expiredAt;
     private int createdAt;
+
+    @Override
+    public String getToken() {
+        return null;
+    }
+
+    @Override
+    public Boolean isExpired() {
+        return null;
+    }
 }
